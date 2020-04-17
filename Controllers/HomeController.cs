9be3dbf168c88abb976073c157fa728edb7bf601
pofider <jan.blaha@hotmail.com>
2020-04-17 21:14:38 +0200

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using jsreport.AspNetCore;
+using jsreport.Types;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [MiddlewareFilter(typeof(JsReportPipeline))]
+        public IActionResult Index()
+        {
+            HttpContext.JsReportFeature().Recipe(Recipe.PhantomPdf);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jsreport-dotnet-example-custom-extension
+
 jsreport .net example with custom extension
+
+1. go to jsreport folder and run `npm i`
+2. start with F5 and the index route gets converted into pdf using phantom-pdf custom recipe
+
